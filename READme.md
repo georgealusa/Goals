7609509487cc63c93f452a6c06d148ff08793803
georgealusa <georgealusa@gmail.com>
2021-08-02 11:43:06 +0300

    initial commit

---
+++ READme.md
@@ -0,0 +1,56 @@
+# Password Locker
+### by :George Alusa
+
+#### To clone project to your local computer and run it follow the following instructions.
+
+### Setup
+> Install these first:
+* Python3.8
+* pip
+* pyperclip
+* xclip
+
+ 
+
+ >1. `` git clone & git remote add origin https://github.com/georgealusa/password-locker.git``
+>2. ``cd password``
+>3. To run the app,  use  ``` python3.8 password.py ```
+>4. To run tets use ``` python3.8 user_credentials_test.py ```
+
+
+
+
+## Technologies used
+* Python3.8
+
+
+| Behaviour | Input | Output |
+| :---------------- | :---------------: | ------------------: |
+| Display codes for navigation | **In terminal: $./password.py** | Welcome, choose an option: ca-Create Account, li-Log In, ex-Exit |
+| Display prompt for creating an account | **Enter: na** | Enter your first name, last name and password |
+| Display prompt for login in | **Enter: li** | Enter your account name and password |
+| Display codes for navigation | **Successful login** | Choose an option: cc - Create Credential, dc - Display Credentials, copy - Copy Credential, ex - exit |
+| Display prompt for creating a credential | **Enter: cc** | Enter the site name, your username and password |
+| Display a list of credentials | **Enter: dc** | Prints a list of saved credentials |
+| Display prompt for which credential to copy | **Enter: copy** | Enter the site name of the credential you wish to copy. |
+| Exit application | **Enter: ex** | Exit the current navigation stage |
+
+
+### Cloning
+* In your terminal:
+        
+        $ git clone https://github.com/georgealusa/password-locker.git``
+        $ cd password
+
+## Running the Application
+* To run the application, in your terminal:
+
+        $ chmod +x password.py
+        $ ./password.py
+        
+
+
+
+## License
+> MIT License 2021 George Alusa
+
---
+++ user_credentials_test.py
@@ -0,0 +1,125 @@
+import unittest
+import pyperclip
+from user_credentials import User, Credential
+
+class TestUser(unittest.TestCase):
+	'''
+	Test class that defines test cases for the user class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def setUp(self):
+		'''
+		Function for creating a user account before each test
+		'''
+		self.new_user = User('Stephen','Ndele','ndele')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of user instances is properly done
+		'''
+		self.assertEqual(self.new_user.first_name,'Stephen')
+		self.assertEqual(self.new_user.last_name,'Ndele')
+		self.assertEqual(self.new_user.password,'ndele')
+
+	def test_save_user(self):
+		'''
+		Test to check if the new users information is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
+
+class TestCredentials(unittest.TestCase):
+	'''
+	Test class that defines test cases for the credentials class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def test_check_user(self):
+		'''
+		Function to test whether the login in function check_user works as expected
+		'''
+		self.new_user = User('Stephen','Ndele','ndele')
+		self.new_user.save_user()
+		user2 = User('mark','luke','john')
+		user2.save_user()
+
+		for user in User.users_list:
+			if user.first_name == user2.first_name and user.password == user2.password:
+				current_user = user.first_name
+		return current_user
+
+		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+
+	def setUp(self):
+		'''
+		Function to create an account's credentials before each test
+		'''
+		self.new_credential = Credential('Stephen','Facebook','steve','steve')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+		self.assertEqual(self.new_credential.user_name,'Stephen')
+		self.assertEqual(self.new_credential.site_name,'Facebook')
+		self.assertEqual(self.new_credential.account_name,'steve')
+		self.assertEqual(self.new_credential.password,'steve')
+
+	def test_save_credentials(self):
+		'''
+		Test to check if the new credential info is saved into the credentials list
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Stephen','Twitter','steve','steve')
+		twitter.save_credentials()
+		self.assertEqual(len(Credential.credentials_list),2)
+
+	
+	def tearDown(self):
+		'''
+		Function to clear the credentials list after every test
+		'''
+		Credential.credentials_list = []
+		User.users_list = []
+
+	def test_display_credentials(self):
+		'''
+		Test to check if the display_credentials method, displays the correct credentials.
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Stephen','Twitter','steve','steve')
+		twitter.save_credentials()
+		gmail = Credential('Stephen','Gmail','steve','steve')
+		gmail.save_credentials()
+		self.assertEqual(len(Credential.display_credentials(twitter.user_name)),3)
+
+	def test_find_by_site_name(self):
+		'''
+		Test to check if the find_by_site_name method return the correct credential
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Stephen','Twitter','steve','steve')
+		twitter.save_credentials()
+		credential_exists = Credential.find_by_site_name('Twitter')
+		self.assertEqual(credential_exists,twitter)
+
+	def test_copy_credential(self):
+		'''
+		Test to check if the copy a credential method copies the correct credential
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Stephen','Twitter','steve','steve')
+		twitter.save_credentials()
+		find_credential = None
+		for credential in Credential.user_credentials_list:
+				find_credential =Credential.find_by_site_name(credential.site_name)
+				return pyperclip.copy(find_credential.password)
+		Credential.copy_credential(self.new_credential.site_name)
+		self.assertEqual('steve',pyperclip.paste())
+		print(pyperclip.paste())
+
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
